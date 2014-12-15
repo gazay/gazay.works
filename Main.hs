bc41732cea0f900090f148fbfa0e0426fd4a9605
Alexey Gaziev <alex.gaziev@gmail.com>
2014-12-14 22:22:32 -0800

    Add blaze support

--- Main.hs
+++ Main.hs
@@ -1,6 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 import Web.Scotty
+import Text.Blaze.Html5
+import Text.Blaze.Html5.Attributes
+import qualified Web.Scotty as S
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Html.Renderer.Text
 
 main = scotty 3000 $ do
   get "/" $ do
-    html "Hello World!"
+    S.html . renderHtml $ do
+      h1 "Hello World!"
