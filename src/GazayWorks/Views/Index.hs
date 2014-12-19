ca3402d98d16add809bcadf2e2a8099e97ad759c
Alexey Gaziev <alex.gaziev@gmail.com>
2014-12-19 21:27:42 +0800

    Not working =(

--- src/GazayWorks/Views/Index.hs
+++ src/GazayWorks/Views/Index.hs
@@ -1,13 +1,18 @@
 module GazayWorks.Views.Index where
 
+import Text.Blaze.Html5
+import Text.Blaze.Html5.Attributes
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+
 render = do
-  html $ do
-    body $ do
+  H.html $ do
+    H.body $ do
       h1 "Alexey Gaziev"
       ul $ do
         li $ do
-          span "first work"
+          H.span "first work"
           img ! src "http://41.media.tumblr.com/95df8f466cec202e9c1d940070fdc147/tumblr_ng9hzwypw11u4la5zo1_1280.jpg"
         li $ do
-          span "second work"
+          H.span "second work"
           img ! src "asdfas"
